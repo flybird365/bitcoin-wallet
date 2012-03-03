a5973f64a44f03e078719f7e1817e056b5850a46
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version

--- wallet/src/de/schildbach/wallet/ui/ExchangeRatesFragment.java
+++ wallet/src/de/schildbach/wallet/ui/ExchangeRatesFragment.java
@@ -57,7 +57,7 @@ import de.schildbach.wallet.WalletApplication;
 import de.schildbach.wallet.WalletBalanceWidgetProvider;
 import de.schildbach.wallet.service.BlockchainService;
 import de.schildbach.wallet.util.WholeStringBuilder;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 /**
  * @author Andreas Schildbach
